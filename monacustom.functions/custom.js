6b3c0fa3a86d439486add85f2f0b54e4449cae64
Ramona Saintandre <codinggrandma@gmail.com>
2019-12-03 09:04:50 -0500

    creating declartion functions, issue with return

--- monacustom.functions/custom.js
+++ monacustom.functions/custom.js
@@ -1,5 +1,12 @@
-'use strict';
+// 'use strict';
 
-function calBill (){
-    console.log ('running calBill');
+function calBill() {
+    console.log('running calBill');
+    const total = 100 * .15;
+    return total;
+
 }
+const billTotal = calBill();
+console.log('this is the total bill');
+//This shows up as undefined because it is out of scope 
+// console.log(total);
