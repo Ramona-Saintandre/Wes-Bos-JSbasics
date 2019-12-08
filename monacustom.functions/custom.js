350ac0a62b7ad5c56a44de5d1cafbf090d145a41
Ramona Saintandre <codinggrandma@gmail.com>
2019-12-07 23:32:07 -0500

    trying to figure out why it wont output to console

--- monacustom.functions/custom.js
+++ monacustom.functions/custom.js
@@ -1,12 +1,8 @@
 // 'use strict';
+function calBill(){
+    console.log('Running Calculate Bill!!');
+    const total = bill * 1 + taxRate;
+    return total;
+}
 
-function calBill() {
-    // console.log('running calBill');
-    const total = 100 * .15;
-    return total; //will not return, need to work on this
-
-};
 const billTotal = calBill();
-console.log(`this is the total bill $${billTotal}`);
-//This shows up as undefined because it is out of scope 
-// console.log(total);
